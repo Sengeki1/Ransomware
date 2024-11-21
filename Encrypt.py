58e586e81493f4aeab2bb63345a459ad6645efe8
Sengeki1 <gamersengeki@gmail.com>
2024-11-21 12:09:41 -0100

    Decrypthion

--- Encrypt.py
+++ Encrypt.py
@@ -8,14 +8,16 @@ for file in os.listdir("files"):
     if os.path.isfile(full_path):
         files.append(file)
 
-with open("Key.key", "rb") as key_:
-    secret_key = key_.read()
+key = Fernet.generate_key()
+
+with open("Key.key", "wb") as key_:
+    key_.write(key)
 
 for file in files:
     full_path = os.path.join("files", file)
     with open(full_path, "rb") as thefile:
         contents = thefile.read()
-    contents_decrypthed = Fernet(secret_key).decrypt(contents) # Encrypth content of the file
+    contents_encrypthed = Fernet(key).encrypt(contents) # Encrypth content of the file
 
     with open(full_path, "wb") as thefile:
-        contents = thefile.write(contents_decrypthed)
+        contents = thefile.write(contents_encrypthed)
